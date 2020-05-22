874eaa1b2765bfab3843f7730d2e2cdc0d3202c2
ahmetruhi <58765784+ahmetruhi@users.noreply.github.com>
2020-05-22 17:42:42 +0300

    Kitabevi Vize Çalışması

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitabevi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap[] Kitabevi = new Kitap[0];
+            string cik = "";
+            while (cik != "E")
+            {
+                Console.WriteLine("Kitabevi uygulamasına hoşgeldiniz.");
+                Console.Write("Kitap eklemek için 1'e\nKitap listesini görmek için 2'ye basınız ");
+                int list = int.Parse(Console.ReadLine());
+                if (list == 1)
+                {
+                    try
+                    {
+                        Console.WriteLine("Kaç Kitap Eklemek İstiyorsunuz?");
+                        byte kitapSecim = byte.Parse(Console.ReadLine());
+                        Kitabevi = new Kitap[kitapSecim];
+                        for (int i = 0; i < kitapSecim; i++)
+                        {
+                            try
+                            {
+                                Kitap kitap = new Kitap();
+                                Console.WriteLine("Lütfen {0}. kitap bilgilerini ekleyiniz", i + 1);
+                                Console.Write($"Kitabın adını giriniz: ");
+                                kitap.Kitaptitle = Console.ReadLine().ToUpper();
+                                Console.Write($"Kitabın yazarını giriniz: ");
+                                kitap.Kitapyazar = Console.ReadLine();
+                                Console.Write($"Kitabın türünü giriniz: ");
+                                kitap.Kitaptur = Console.ReadLine();
+                                Console.Write($"Kitabın basım yılını giriniz: ");
+                                kitap.Kitaptarih = new DateTime(Convert.ToInt32(Console.ReadLine()), 1, 1);
+                                Console.WriteLine("--------------------------------------");
+                                Kitabevi[i] = kitap;
+                                Kitap.dosyaYaz(kitap.Kitaptitle, kitap.Kitapyazar, kitap.Kitaptur, kitap.Kitaptarih);
+                            }
+                            catch (Exception hata)
+                            {
+                                Console.WriteLine("Şöyle bir hata var: " + hata.Message);
+                            }
+                        }
+                    }
+                    catch (Exception hata)
+                    {
+                        Console.WriteLine("Şöyle bir hata var: " + hata.Message);
+                    }
+                }
+                else if (list == 2)
+                {
+                    Kitap.Kitaplar();
+                }
+                else
+                {
+                    Console.WriteLine("\nHatalı Girdiniz Lütfen Tekrar Deneyiniz.\n");
+                }
+                Console.WriteLine("Programdan Çıkmak İstiyor musunuz? (E/H):");
+                cik = Console.ReadLine().ToUpper();
+            }
+        }
+    }
+}
---
+++ kitap.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitabevi
+{
+    public class Kitap : Program
+    {
+        public string Kitaptitle { get; set; }
+        public string Kitapyazar { get; set; }
+        public string Kitaptur { get; set; }
+
+        private DateTime _kitaptarih;
+        public DateTime Kitaptarih
+        {
+            get { return _kitaptarih; }
+            set
+            {
+                if (value.Year >= 1900 && value.Year <= DateTime.Now.Year)
+                {
+                    _kitaptarih = value;
+                }
+                else
+                {
+                    YilKontrol();
+                }
+            }
+        }
+
+        private void YilKontrol()
+        {
+            while (!(_kitaptarih.Year >= 1000 && _kitaptarih.Year <= DateTime.Now.Year))
+            {
+                Console.Write("Hatalı Yıl! Lütfen basım yılını bir daha giriniz: ");
+                _kitaptarih = new DateTime(Convert.ToInt32(Console.ReadLine()), 1, 1);
+            }
+        }
+
+
+        private static string dosyaYol = AppDomain.CurrentDomain.BaseDirectory + "kitaplist.txt";
+        public static void Kitaplar()
+        {
+            if (File.Exists(dosyaYol))
+            {
+                string bilgileriOku = File.ReadAllText(dosyaYol);
+                Console.WriteLine(bilgileriOku);
+            }
+        }
+
+
+        public static void dosyaYaz(string kitaptitle, string kitapyazar, string kitaptur, DateTime kitaptarih)
+        {
+            string kitapBilgi = "\n-Kitap Bilgileri-\nKitap adı: " + kitaptitle + "\nKitap Yazarı: " + kitapyazar + "\nKitap Türü: " + kitaptur + "\nKitap Basım Tarihi: " + kitaptarih.Year;
+            File.AppendAllText(dosyaYol, kitapBilgi + Environment.NewLine);
+        }
+
+    }
+}
